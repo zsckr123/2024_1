d7ca0649949961a9564d7db58f8bda2b6515e71b
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-09-12 08:50:42 +0200

    v1

--- 2tz5_1/algorytm_euklidesa.py
+++ 2tz5_1/algorytm_euklidesa.py
@@ -1,6 +1,8 @@
 ''' Dzielimy liczba1 przez liczba2 '''
 liczba1 = int(input('podaj pierwszą liczbę: '))
 liczba2 = int(input('podaj drugą liczbę: '))
+a = liczba1
+b = liczba2
 licznik = 0
 if liczba2 == 0:
       print("NWD to:",liczba1)
@@ -12,3 +14,6 @@ else:
 
 # koniec pętli
 print("najwiekszy wspolny dzielnik to:",liczba1)
+nww = a*b / liczba1
+print(f'NWW {a} i {b} = {nww} ')
+# NWW(a,b) = a * b / NWD(a,b)
