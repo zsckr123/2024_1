86b4adec970560fb766bb57136bc7b9e4dfe7216
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-09-12 09:37:03 +0200

    v1

--- 2TMR_1/algorytm_euklidesa.py
+++ 2TMR_1/algorytm_euklidesa.py
@@ -2,6 +2,8 @@
 while True:
     liczba1 = int(input('podaj pierwszą liczbę: '))
     liczba2 = int(input('podaj drugą liczbę: '))
+    a = liczba1
+    b = liczba2
 
     if liczba2 == 0:
           print("NWD = ", liczba1)
@@ -11,6 +13,9 @@ while True:
             liczba1 = liczba2
             liczba2 = reszta
         print("najwiekszy wspolny dzielnik to:", liczba1)
+        nww =  a * b / liczba1
+        print(f' NWW({a},{b})={nww}')
+
     key = input('Podaj x aby zakończyć')
     if key == 'x':
         break
