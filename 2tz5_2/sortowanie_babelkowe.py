9cea3ef244bc2c5cf73a0a98f6871fca737796ac
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-10-21 08:32:05 +0200

    v1

--- 2tz5_2/sortowanie_babelkowe.py
+++ 2tz5_2/sortowanie_babelkowe.py
@@ -2,25 +2,29 @@ import time
 import random
 def babelek(t):
   for i in range(len(t)-1):
-    if t[i] > t[i+1]:
+    if t[i] < t[i+1]:
       t[i], t[i+1] = t[i+1], t[i] #  przestawienie elementów
   return t
 
 
 def sort_b(t):
-    start = time.time()
+
     for j in range(len(t)):
         babelek(t)
-    stop = time.time()
-    delta = stop - start
-    return t, delta
+
+    return t
 
 lista = []
-for _ in range(100000):
+for _ in range(20000):
     liczba = random.randint(-100000,100000)
     lista.append(liczba)
 
 # print(sorted(lista,reverse=True))
 print('Lista nieposortowana ', lista)
-print(sort_b(lista)[0])
-print('Czas wykonania ', sort_b(lista)[1])
+print('start')
+start = time.time()
+print(sort_b(lista))
+print('stop')
+stop = time.time()
+delta = stop - start
+print('Czas wykonania ', delta)
