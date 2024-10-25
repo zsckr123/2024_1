18aedb364350911068f91a8f11f032b4033fb2ff
fedora <miroslaw.butajlo@estudzieniec.pl>
2024-10-25 10:26:25 +0200

    v1

---
+++ 2tz5_2/porowanie_metod.py
@@ -0,0 +1,37 @@
+import time
+import random
+from audioop import reverse
+from random import random, randint
+
+
+def sort_insert(A):
+
+    for i in range(1,len(A)):
+        klucz = A[i]
+        j = i - 1
+        while j >= 0 and A[j] < klucz: # zmiana znaku = zmiana typu sortowania
+            A[j + 1] = A[j]
+            j = j - 1
+        A[j + 1] = klucz
+
+    return A
+
+lista =[]
+for _ in range(10000):
+    liczba = randint(-10000,10000)
+    lista.append(liczba)
+print('Lista nieposortowana')
+print(20*'-')
+print(lista)
+print('Lista posortowana za pomocą metody przez wstawianie')
+print(20*'-')
+start = time.time()
+print(sort_insert(lista))
+stop = time.time()
+print("czas sortowania", stop - start)
+
+print('Lista posortowana za pomocą metody sorted')
+start = time.time()
+print(sorted(lista, reverse=False))
+stop = time.time()
+print("czas metody sorted", stop - start)
