fda886025c9a78bd1a8438565c279bb6af3569d2
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-02-19 09:28:29 +0100

    v2

--- 1RZ_tr/dzialania_arytmetyczne.py
+++ 1RZ_tr/dzialania_arytmetyczne.py
@@ -1,5 +1,9 @@
-a = int(input('Podaj pierwszę liczbę'))
-b = int(input('Podaj drugą liczbę'))
-print(f'Wynik dodawania {a+b}')
-print(f'Wynik odejmowania {a-b}')
-print(f'Wynik mnożenia {a*b}')
+a = int(input('Podaj pierwszę liczbę='))
+b = int(input('Podaj drugą liczbę='))
+print(f'Wynik dodawania= {a+b}')
+print(f'Wynik odejmowania= {a-b}')
+print(f'Wynik mnożenia= {a*b}')
+if b==0:
+    print('Dzielnie nie wykonalne')
+else:
+    print(f'Wynik dzielenia= {a/b}')
