774793948539c157f3c7850c5d9729f481fecb82
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-02-24 14:15:00 +0100

    v2

--- 1tmr_1/dzialania_arytmetyczne.py
+++ 1tmr_1/dzialania_arytmetyczne.py
@@ -3,3 +3,7 @@ b = int(input('Podaj drugą liczbę '))
 print('suma liczb',a + b)
 print('odejmowanie liczb',a - b)
 print('mnożenie liczb',a * b)
+if b==0:
+    print("dzielenie niewykonalne")
+else:
+    print('Iloraz liczb a i b =', a / b)
