c0c1086bd3a8ab0ac320c09f790b3ff967ec3612
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-02-24 09:03:43 +0100

    v2

--- 1TW_1/dzialnia_na_liczbach.py
+++ 1TW_1/dzialnia_na_liczbach.py
@@ -1,3 +1,9 @@
-a = int(input('Podaj pierwszą liczbę'))
-b =  int(input('Podaj drugą liczbę'))
+a = int(input('Podaj pierwszą liczbę: '))
+b =  int(input('Podaj drugą liczbę: '))
 print('Suma liczb a i b =',a+b)
+print('Rożnica liczb a i b =',a-b)
+print('Iloczyn liczb a i b =',a*b)
+if b==0:
+    print('Dzielenie przez zero niewykoanlne')
+else:
+    print('Iloraz liczb a i b =',a/b)
