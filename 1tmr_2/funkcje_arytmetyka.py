6ad46593939769905f804dd8db2456920b98bf50
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-02-28 08:29:11 +0100

    v2

---
+++ 1tmr_2/funkcje_arytmetyka.py
@@ -0,0 +1,10 @@
+def arytmetyka(a, b):
+    print('Suma ',a+b)
+    print('Rożnica ',a-b)
+    print('Iloczyn ',a*b)
+    if b==0:
+        print('Dzielenie niewykonalne')
+    else:
+        print('Iloraz ', a / b)
+
+arytmetyka(2,5)
