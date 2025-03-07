0780ec32a0f052fc76b6f75c8cf405339a3d0f35
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-03-07 08:22:17 +0100

    v2

--- 1tmr_1/pole_kwadratu.py
+++ 1tmr_1/pole_kwadratu.py
@@ -1,7 +1,7 @@
 ''' Pole kwadratu'''
 
 def pole_kwadratu( bok):
-    return bok ** 2
+    return bok**2
 
 a = int(input('podaj długość boku '))
 print('Pole wynosi' , pole_kwadratu(a))
---
+++ 1tmr_1/pole_prostokata.py
@@ -0,0 +1,7 @@
+''' Pole kwadratu'''
+
+def pole_prostokata( a, b):
+    return a*b
+
+# a = int(input('podaj długość boku '))
+print('Pole wynosi' , pole_prostokata(20,3))
