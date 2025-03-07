90cb10e28b4627e4da7e62065c89730e35e68b6f
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-03-07 08:37:53 +0100

    v2

---
+++ 1tmr_1/pole_prostokata_ver2.py
@@ -0,0 +1,9 @@
+''' Pole prostokąta'''
+
+def pole_prostokata( a, b):
+    return a*b
+
+a=float(input('Podaj pierwszy bok:'))
+b=float(input('Podaj pierwszy bok:'))
+
+print('Pole wynosi' , pole_prostokata(a,b))
