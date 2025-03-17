14e646c857be936b3db72246e8aebc66c80857d1
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-03-17 14:31:55 +0100

    v2

--- 1tmr_1/drukuj_malymi_literami.py
+++ 1tmr_1/drukuj_malymi_literami.py
@@ -1,7 +1,5 @@
 def drukuj_malymi_literami(text):
     return text.lower()
 
-
-
-a =input('Podaj tekst wielkimi literami: ')
+a = input('Podaj tekst dużymi literami: ')
 print(drukuj_malymi_literami(a))
