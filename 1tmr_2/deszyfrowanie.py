da37d4434098e90397a411a55498fab3fa784442
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-04-11 08:28:24 +0200

    v3

--- 1tmr_2/deszyfrowanie.py
+++ 1tmr_2/deszyfrowanie.py
@@ -1,7 +1,6 @@
-print(chr(76))
+# print(chr(76))
 
-lista = [115, 99, 104, 111, 119, 97, 106]
-
+lista = [112, 117, 115, 116, 97, 32, 103, 108, 111, 119, 97]
 tmp = ''
 for znak in lista:
     tmp += chr(znak) # tmp = tmp + chr(znak)
