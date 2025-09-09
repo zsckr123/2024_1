5cc0b7f1f3bee7643e7386c046d884cd99673f08
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-09-09 09:22:06 +0200

    v3

---
+++ 2025/dzialnia_na_liczbach_calkowitycg.py
@@ -0,0 +1,7 @@
+
+pierwsza_liczba = int(input('Podaj pierwsza liczbe'))
+druga_liczba = int(input('Podaj drugą liczbe'))
+reszta = pierwsza_liczba % druga_liczba
+dziel_calk = pierwsza_liczba // druga_liczba
+print(f'reszta z dzielenia = {reszta}')
+print(f' dzielnie całkowite={dziel_calk}')
