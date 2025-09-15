127b4827b66a87c9d2fa04377458844da412a591
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-09-15 12:20:26 +0200

    v3

---
+++ 2025/2TMZ_2/dzialnia_na_liczbach_calkowitycg.py
@@ -0,0 +1,15 @@
+
+pierwsza_liczba = int(input('Podaj pierwsza liczbe='))
+druga_liczba = int(input('Podaj drugą liczbe='))
+reszta = pierwsza_liczba % druga_liczba
+dziel_calk = pierwsza_liczba // druga_liczba
+print(f'reszta z dzielenia = {reszta}')
+print(f' dzielnie całkowite={dziel_calk}')
+
+#  5 % 2 = 1  5 modulo 2  inaczej reszta z dzielenia 5 przez 2 
+# 5 // 2 - dzielenie całkowite cyfra przed kropką dziesiętną
+# 5 / 2 = 2.5 
+# 5 // 2 = 2
+# 23 // 3 = 7
+# 23 % 3 = 2
+# 23 / 3 = 7 reszta 2
