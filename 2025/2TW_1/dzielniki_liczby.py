be815049fac78a4c48d7693c225398e67bde84ac
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-09-22 11:13:51 +0200

    v3

--- 2025/2TW_1/dzielniki_liczby.py
+++ 2025/2TW_1/dzielniki_liczby.py
@@ -1,7 +1,7 @@
 
 liczba = int(input('Podaj liczbe'))
 lista_dzielnikow = []
-for i in range(1,liczba):
+for i in range(2,int(round(liczba**0.5,0)) +1):
     if liczba % i == 0:
         lista_dzielnikow.append(i)
 print(f'Lista dzielnikow liczby {liczba} wynosi {lista_dzielnikow}')
