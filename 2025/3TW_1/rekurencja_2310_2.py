674cbc5d096fc1d8528a7909c353420f70b1ddb3
fedora <miroslaw.butajlo@estudzieniec.pl>
2025-10-23 09:19:25 +0200

    rekurencja

--- 2025/3TW_1/rekurencja_2310_2.py
+++ 2025/3TW_1/rekurencja_2310_2.py
@@ -1,9 +1,9 @@
 def a(n):
     if n==1:
-        return 1
-    if n==2:
         return 2
-    return a(n-1)/a(n-2)
+    if n==2:
+        return 4
+    return (a(n-1)+a(n-2))/2
 
 for i in range(1,21):
     print(f'a({i})={a(i)}')
